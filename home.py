d2cdb88055577294f8d54a5b8f1f8b2f859d2a9e
Kunal Dalvi <kunald9960@gmail.com>
2024-05-08 12:14:06 +0530

    Remove Extra spaces and lines

--- home.py
+++ home.py
@@ -29,7 +29,6 @@ def V_SPACE(lines):
 PAGE_TITLE = "Digital CV | Kunal Dalvi"
 PAGE_ICON = "assets/favicon.ico"
 
-
 st.set_page_config(page_title=PAGE_TITLE, page_icon=PAGE_ICON)
 
 # This is for scrolling at top
@@ -47,7 +46,6 @@ Hi! I'm Kunal Dalvi, a Python enthusiast with a keen interest in data analysis.
 I'm driven by curiosity and love taking on new challenges. 
 I'm dedicated to unravel insights from complex datasets through statistical methods and programming.
 """
-
 
 SOCIALS = (
     '<a href="https://www.linkedin.com/in/kunal-dalvi-0b273b2b4"><img src="https://img.icons8.com/external-justicon-lineal-color-justicon/344/external-linkedin-social-media-justicon-lineal-color-justicon.png" width="60"/></a>',
@@ -76,7 +74,7 @@ with st.sidebar:
                          styles={'navtab': {'background-color': '#111',
                                             'color': '#818181',
                                             'font-size': '20px',
-                                            'transition': '.1s',
+                                            'transition': '.01s',
                                             'white-space': 'nowrap',
                                             'text-transform': 'uppercase',
                                             'font': 'consolas'},
@@ -87,7 +85,8 @@ with st.sidebar:
                                                'text-align': 'left'},
                                  'tabStyle': {'list-style-type': 'none',
                                               'margin-bottom': '30px',
-                                              'padding-left': '30px'}},
+                                              'padding-left': '30px'
+                                              }},
                          key="1", default_choice=0)
 
 
@@ -99,6 +98,7 @@ if tabs == 'Home':
         st.components.v1.html(js)
         time.sleep(.1)
     temp.empty()
+
     st.markdown(
         """
         <script>
@@ -268,7 +268,7 @@ if tabs == 'Home':
         )
         st.markdown('<div class="vertical-line"></div>', unsafe_allow_html=True)
 
-    # --- EXPERIENCE AND QUALIFICATIONS
+    # --- EXPERIENCE AND QUALIFICATIONS ---
 
     V_SPACE(1)
     V_SPACE(1)
@@ -307,7 +307,7 @@ if tabs == 'Home':
     with col3:
         st.write("77.8%")
 
-    # --- CONNECT WITH ME
+    # --- CONNECT WITH ME ---
 
     V_SPACE(1)
     V_SPACE(1)
@@ -335,7 +335,8 @@ From: {user_email}
             "https://steamuserimages-a.akamaihd.net/ugc/1696157019707375037/BD6E6F9F1065D25D25E63DADF820A406B28032BA/?imw=5000&imh=5000&ima=fit&impolicy=Letterbox&imcolor=%23000000&letterbox=false")
         st.write("<i>\"Sometimes the greatest dishes come from the most unlikely ingredients\"</i> - Po",
                  unsafe_allow_html=True)
-        st.markdown("[![Visitors](https://api.visitorbadge.io/api/visitors?path=https%3A%2F%2Fportfolio-kunal.streamlit.app%2F&label=WEBSITE%20VIEWS&labelColor=%23333333&countColor=%23cbc3e3&labelStyle=upper)](https://visitorbadge.io/status?path=https%3A%2F%2Fportfolio-kunal.streamlit.app%2F)")
+        st.markdown(
+            "[![Visitors](https://api.visitorbadge.io/api/visitors?path=https%3A%2F%2Fportfolio-kunal.streamlit.app%2F&label=WEBSITE%20VIEWS&labelColor=%23333333&countColor=%23cbc3e3&labelStyle=upper)](https://visitorbadge.io/status?path=https%3A%2F%2Fportfolio-kunal.streamlit.app%2F)")
 
 
 # --- PAGE 2 ---
@@ -681,10 +682,8 @@ if tabs == 'About':
 
     st.subheader("✍ Languages Used")
     st.write("---")
-    # Define the columns for row 1
     col1, col2, col3, col4, col5, col6 = st.columns([1, 2, 1, 2, 1, 2])
 
-    # Display Python badge and version
     with col1:
         st.image("https://raw.githubusercontent.com/devicons/devicon/master/icons/python/python-original.svg", width=70)
     with col2:
@@ -692,7 +691,6 @@ if tabs == 'About':
         st.markdown(
             "[![Python](https://img.shields.io/badge/Python-3.12-8a2be2.svg?style=flat&logo=python&logoColor=white)](https://www.python.org)")
 
-    # Display CSS badge and version
     with col3:
         st.image("https://raw.githubusercontent.com/devicons/devicon/master/icons/css3/css3-original-wordmark.svg",
                  width=68)
@@ -701,7 +699,6 @@ if tabs == 'About':
         st.markdown(
             "[![CSS](https://img.shields.io/badge/CSS-3.0-1572B6.svg?style=flat&logo=CSS3&logoColor=white)](https://www.w3.org/Style/CSS/Overview.en.html)")
 
-    # Display HTML badge and version
     with col5:
         st.image("https://raw.githubusercontent.com/devicons/devicon/master/icons/html5/html5-original-wordmark.svg",
                  width=68)
@@ -710,32 +707,25 @@ if tabs == 'About':
         st.markdown(
             "[![HTML](https://img.shields.io/badge/HTML-5.0-E34F26.svg?style=flat&logo=html5&logoColor=white)](https://www.w3.org/TR/html52/)")
 
-    # Define the column for the combined progress bar
     col4 = st.columns(1)[0]
 
-    # Display combined progress bar for Python (70%), CSS (20%), and HTML (10%)
     with col4:
         st.markdown(generate_combined_progress_bar(70, 20, 10), unsafe_allow_html=True)
 
     V_SPACE(1)
-    st.subheader("📌 Resources I used")
+    st.subheader("📌 Resources Used")
     st.write("---")
 
-    import streamlit as st
-
-    # Define the URLs of the YouTube videos
     youtube_video_urls = [
         "https://www.youtube.com/embed/hEPoto5xp3k",
-        "https://www.youtube.com/embed/RW8b-lxCm_8"  # Example video, replace with your desired videos
+        "https://www.youtube.com/embed/RW8b-lxCm_8"
     ]
 
-    # Generate the HTML code for embedding the YouTube videos
     youtube_embed_codes = [
         f'<iframe width="100%" height="315" src="{url}" frameborder="0" allowfullscreen></iframe>'
         for url in youtube_video_urls
     ]
 
-    # Display the embedded YouTube videos in two columns
     col1, col2 = st.columns(2)
     with col1:
         st.markdown(youtube_embed_codes[0], unsafe_allow_html=True)
@@ -743,52 +733,57 @@ if tabs == 'About':
     with col2:
         st.markdown(youtube_embed_codes[1], unsafe_allow_html=True)
 
-    import streamlit as st
+    github_logo = '<a href="https://github.com/kunal9960"><img src="https://raw.githubusercontent.com/kunal9960/kunal9960/main/github.png" width="120"/></a>'
 
-    # GitHub logo
-    github_logo = '<a href="https://github.com/kunal9960"><img src="https://raw.githubusercontent.com/kunal9960/kunal9960/main/github.png" width="150"/></a>'
-
-    # Dictionary of repository names and their corresponding links
     repo_links = {
         "Let it Rain": "https://arnaudmiribel.github.io/streamlit-extras/extras/let_it_rain/",
         "Streamlit on Hover Tabs": "https://github.com/Socvest/streamlit-on-Hover-tabs",
-        "Streamlit Option Menu": "https://github.com/victoryhb/streamlit-option-menu"
+        "Streamlit Option Menu": "https://github.com/victoryhb/streamlit-option-menu",
+        "Bootstrap Carousal": "https://github.com/thomasbs17/streamlit-contributions/tree/master/bootstrap_carousel",
+        "Streamlit Extras": "https://github.com/arnaudmiribel/streamlit-extras"
     }
 
-    # Streamlit logo and corresponding links
-    streamlit_logo = '<a href="https://streamlit.io"><img src="https://raw.githubusercontent.com/github/explore/968d1eb8fb6b704c6be917f0000283face4f33ee/topics/streamlit/streamlit.png" width="150"/></a>'
+    streamlit_logo = '<a href="https://streamlit.io"><img src="https://raw.githubusercontent.com/github/explore/968d1eb8fb6b704c6be917f0000283face4f33ee/topics/streamlit/streamlit.png" width="120"/></a>'
     streamlit_links = {
-        "Discussion 1": "https://discuss.streamlit.io/t/create-empty-space-to-separate-portions-of-the-app/8689",
-        "Discussion 2": "https://discuss.streamlit.io/t/how-to-plot-a-matplotlib-animation-in-my-streamlit-app/22764",
-        "Streamlit API Reference": "https://docs.streamlit.io/develop/api-reference/layout"
+        "Empty Spaces": "https://discuss.streamlit.io/t/create-empty-space-to-separate-portions-of-the-app/8689",
+        "Animations": "https://discuss.streamlit.io/t/how-to-plot-a-matplotlib-animation-in-my-streamlit-app/22764",
+        "Streamlit API Reference": "https://docs.streamlit.io/develop/api-reference/layout",
+        "Navigation Bar": "https://discuss.streamlit.io/t/new-component-streamlit-navigation-bar/66032",
+        "Components": "https://docs.streamlit.io/develop/concepts/custom-components/create"
     }
 
     col5, col6 = st.columns(2)
-    # Display in columns
-    col1, col2, col3, col4 = st.columns([1, 1, 1, 1])
+    col1, col2, col8, col3, col4, = st.columns([3, 3, 1, 3, 2])
 
-    # Column 1: GitHub logo
     with col5:
-        st.write("<h4>🔗 Github Repo</h4>", unsafe_allow_html=True)
+        st.write("<h4>🔗 Github Repos</h4>", unsafe_allow_html=True)
         with col1:
             st.markdown(github_logo, unsafe_allow_html=True)
 
-        # Column 2: GitHub repositories with names
         with col2:
             for repo_name, repo_link in repo_links.items():
                 st.markdown(f"**[{repo_name}]({repo_link})**")
+
+        with col8:
+            st.markdown(
+                """
+                <style>
+                    .vertical-line {
+                        border-left: 6px solid #CBC3E3;
+                        height: 230px;
+                        margin: auto;
+                    }
+                </style>
+                """
+                , unsafe_allow_html=True
+            )
+            st.markdown('<div class="vertical-line"></div>', unsafe_allow_html=True)
 
     with col6:
         st.write("<h4>🔗 Streamlit</h4>", unsafe_allow_html=True)
         with col3:
             st.markdown(streamlit_logo, unsafe_allow_html=True)
 
-        # Column 4: Streamlit links with names
         with col4:
             for link_name, link_url in streamlit_links.items():
                 st.markdown(f"**[{link_name}]({link_url})**")
-
-
-
-
-
