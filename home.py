4c587bc349c2451e3e13285741e54f401a4b3026
Kunal Dalvi <kunald9960@gmail.com>
2024-05-08 19:34:33 +0530

    Update for the site

--- home.py
+++ home.py
@@ -869,8 +869,8 @@ top:150px;
 bottom: 0;
 width: 100%;
 background-color: transparent;
-color: #808080; /* theme's text color hex code at 50 percent brightness*/
-text-align: left; /* you can replace 'left' with 'center' or 'right' if you want*/
+color: #808080;
+text-align: left;
 }
 </style>
 
@@ -878,7 +878,8 @@ text-align: left; /* you can replace 'left' with 'center' or 'right' if you want
 
 <div class="footer">
 <p style='font-size: 1em;'>Made with <a style='display: inline; text-align: left;' href="https://streamlit.io/" target="_blank">Streamlit</a><br 'style= top:3px;'>
-with <img src="https://em-content.zobj.net/source/skype/289/red-heart_2764-fe0f.png" alt="heart" height= "10"/><a style='display: inline; text-align: left;' href="https://github.com/kunal9960" target="_blank"> by Kunal</a></p>
+with <img src="https://em-content.zobj.net/source/skype/289/red-heart_2764-fe0f.png" alt="heart" height= "10"/><a style='display: inline; text-align: left;' href="https://github.com/kunal9960" target="_blank"> by Kunal</a>
+<a style='display: inline; text-align: left;'>© Copyright 2024</a></p>
 </div>
 
 </div>
